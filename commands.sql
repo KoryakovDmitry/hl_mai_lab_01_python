2ec5f9bb53a942f98c51eac66041a761bf70f5ad
dimitrii <dm.koryakov@gmail.com>
2023-11-24 00:04:29 +0100

    commands.sql

--- commands.sql
+++ commands.sql
@@ -1,59 +1,61 @@
-CREATE TABLE IF NOT EXISTS User (
-    id INT NOT NULL AUTO_INCREMENT,
-    login VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    first_name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    last_name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    email VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY unique_login (login)
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(256) UNIQUE NOT NULL,
+    first_name VARCHAR(256),
+    last_name VARCHAR(256),
+    email VARCHAR(256),
+    INDEX (login)
 );
 
-CREATE TABLE IF NOT EXISTS Service (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    description TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-    cost DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (id)
-);
-CREATE TABLE IF NOT EXISTS `Order` (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    service_id INT NOT NULL,
-    date_created DATE NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (service_id) REFERENCES Service(id)
+CREATE TABLE IF NOT EXISTS services (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    description TEXT,
+    cost DECIMAL(10, 2),
+    INDEX (name)
 );
 
+CREATE TABLE IF NOT EXISTS orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    INDEX (user_id)
+);
 
-INSERT INTO User (login, first_name, last_name, email)
-VALUES ('john123',  'John', 'Doe', 'john@example.com');
+CREATE TABLE IF NOT EXISTS order_service (
+    order_id INT,
+    service_id INT,
+    PRIMARY KEY (order_id, service_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (service_id) REFERENCES services(id)
+);
 
-INSERT INTO User (login, first_name, last_name, email)
+INSERT INTO users (login, first_name, last_name, email)
+VALUES ('john123', 'John', 'Doe', 'john@example.com');
+
+INSERT INTO users (login, first_name, last_name, email)
 VALUES ('jane567', 'Jane', 'Smith', 'jane@example.com');
 
-INSERT INTO User (login, first_name, last_name, email)
+INSERT INTO users (login, first_name, last_name, email)
 VALUES ('mike789', 'Mike', 'Johnson', 'mike@example.com');
 
-INSERT INTO Service (name, description, cost)
+
+INSERT INTO services (name, description, cost)
 VALUES ('Web Development', 'Professional website development services', 250.00);
 
-INSERT INTO Service (name, description, cost)
+INSERT INTO services (name, description, cost)
 VALUES ('Graphic Design', 'Creative graphic design services', 150.00);
 
-INSERT INTO Service (name, description, cost)
+INSERT INTO services (name, description, cost)
 VALUES ('Digital Marketing', 'Effective online marketing strategies', 300.00);
 
+INSERT INTO orders (user_id, date_created)
+VALUES (1, '2023-10-10'), (2, '2023-11-15'), (3, '2023-09-20');
 
-INSERT INTO `Order` (user_id, service_id, date_created)
-VALUES (1, 1, '2023-10-10');
+INSERT INTO order_service (order_id, service_id)
+VALUES (1, 1), (2, 2), (3, 3);
 
-INSERT INTO `Order` (user_id, service_id, date_created)
-VALUES (2, 2, '2023-11-15');
-
-INSERT INTO `Order` (user_id, service_id, date_created)
-VALUES (3, 3, '2023-09-20');
-
-SELECT * FROM User;
-SELECT * FROM `Order`;
-SELECT * FROM Service;
+SELECT * FROM users;
+SELECT * FROM orders;
+SELECT * FROM services;
