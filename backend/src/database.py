283b020a9d54939385268b226516e7385f194a33
dimitrii <dm.koryakov@gmail.com>
2023-11-24 02:29:22 +0100

    upd db

--- backend/src/database.py
+++ backend/src/database.py
@@ -9,9 +9,16 @@ from sqlalchemy.orm import sessionmaker
 # Load environment variables
 load_dotenv()
 
+# Database connection details
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", 3360)
+DB_USER = os.getenv("DB_USER", "stud")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "stud")
+DB_DATABASE = os.getenv("DB_DATABASE", "archdb")
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "SQLALCHEMY_DATABASE_URL", "mysql+pymysql://user:password@localhost:3360/dbname"
+
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
